056ccc2d527c6b0ba60fffdb61178742f04fcbb2
ChefServiceInfo <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>
2023-11-30 17:59:28 +0100

    Preparing maven dependencies and plugins. Verifying functionalities by adding new Junit 5 Test.

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Rami SAHRAOUI on 30/11/2023
+ */
+class NotesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+        assertEquals(3, 3);
+    }
+}
